c07db274de275cd46b290a8079fae032ba11f4c1
DevAdedeji <adedejitewogbade2@gmail.com>
2022-03-29 00:57:00 +0100

    completed

--- src/store/index.js
+++ src/store/index.js
@@ -5,28 +5,32 @@ import { providers, Signer, ethers, Wallet } from "ethers"
 export default createStore({
   state: {
     balance: "0",
+    address: "",
+    Notconnected: true,
   },
   actions: {
-    async connectToWallet({ commit, state }, needSigner = false) {
+    //Function to connect the wallet and will return a signer to sign transactons
+    async connectToWallet({ commit, state }) {
       const provider = new ethers.providers.Web3Provider(window.ethereum)
       await provider.send("eth_requestAccounts", [])
-      if (needSigner) {
-        const signer = provider.getSigner()
-        return signer
-      }
-      let balance = await provider.getBalance("ethers.eth")
-
-      balance = ethers.utils.formatEther(balance)
-
-      commit('setBalance', balance)
-
-      return provider
+      const signer = provider.getSigner()
+      const address = await signer.getAddress()
+      let balance = await provider.getBalance(address)
+      let eth = ethers.utils.formatEther(balance)
+      commit('setBalance', eth)
+      commit('setConnected', false)
+      commit('setAddress', address)
+      return signer;
     },
   },
   mutations: {
-    setBalance: (state, balance) => (state.balance = balance)
+    setBalance: (state, balance) => (state.balance = balance),
+    setConnected: (state, connected) => (state.Notconnected = connected),
+    setAddress: (state, address) => (state.address = address)
   },
   getters: {
-    getBalance: (state) => { return state.balance }
+    getBalance: (state) => { return state.balance },
+    getAddress: (state) => { return state.address },
+    getConnect: (state) => { return state.Notconnected }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="home">
-    <button @click="connect">Connect to wallet</button>
-    <p>{{ getBalance }}</p>
+    <div class="connect" v-show="getConnect">
+      <button @click="connect">Connect to wallet</button>
+    </div>
+    <div :class="getConnect ? 'details dont-show' : 'details show'">
+      <div class="account-details">
+        <p>You have:</p>
+        <p id="amt">{{ getBalance }} ETH</p>
+        <p>Your address:</p>
+        <p id="address">{{ getAddress }}</p>
+      </div>
+      <form @submit.prevent="send" class="transfer">
+        <p id="title">Transfer ETH:</p>
+        <div class="address">
+          <label for="address">Receiver's Address:</label>
+          <input type="text" id="receiverAaddress" required />
+        </div>
+        <div class="address">
+          <label for="amount">Amount:</label>
+          <input type="text" id="amount" required />
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import { ethers } from "ethers";
 import { mapActions, mapGetters } from "vuex";
 export default {
   name: "Home",
@@ -14,11 +36,22 @@ export default {
     ...mapActions(["connectToWallet"]),
     connect() {
       this.connectToWallet();
-      // this.$router.push("/details");
+    },
+    async send() {
+      const signer = await this.connectToWallet();
+      let receiverAddress = document.getElementById("receiverAaddress").value;
+      let amount = document.getElementById("amount").value;
+      let tx = {
+        to: receiverAddress,
+        value: ethers.utils.parseEther(amount),
+      };
+      signer.sendTransaction(tx).then((txObj) => {
+        console.log("txHash", txObj.hash);
+      });
     },
   },
   computed: {
-    ...mapGetters(["getBalance"]),
+    ...mapGetters(["getBalance", "getAddress", "getConnect"]),
   },
 };
 </script>
@@ -44,7 +77,73 @@ export default {
   transition: 0.5s all;
 }
 
+.home .details {
+  width: 40%;
+  max-height: 90vh;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.5);
+}
+
+.home .details .account-details {
+  width: 90%;
+  margin: 20px auto;
+  background: black;
+  border-radius: 5px;
+  padding: 10px;
+  color: #fff;
+}
+
+.home .details .account-details #amt {
+  font-size: 25px;
+}
+
+.home .details .transfer {
+  width: 90%;
+  margin: 20px auto;
+}
+
+.home .details .transfer #title {
+  font-size: 20px;
+}
+
+.home .details .transfer div {
+  width: 100%;
+  padding: 10px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.home .details .transfer div input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  outline: none;
+}
+
+.home .details .transfer button {
+  width: 100%;
+  padding: 10px 0;
+}
+
+.home .details.show {
+  display: block;
+}
+
+.home .details.dont-show {
+  display: none;
+}
+
 .home button:focus {
   transform: scale(0.9);
+}
+
+@media (max-width: 900px) {
+  .home .details {
+    width: 90%;
+    margin: 0 auto;
+  }
+  .home .details .account-details #address {
+    font-size: 12px;
+  }
 }
 </style>
